8761c3d734b40391b79318e4ba45a7814f6aab35
iboulangeat <isabelle.boulangeat@irstea.fr>
2023-03-02 15:14:12 +0100

    carte interactive

--- 3bis_plotDistri.R
+++ 3bis_plotDistri.R
@@ -35,6 +35,28 @@ leaflet(sites_all)%>%
   setView(lng=5.5,lat=45,zoom=6) %>%
   addCircleMarkers(lng = ~lon_wgs84, lat = ~lat_wgs84, popup = ~date, radius = 0.3, opacity = 0.8, color = "black") 
 
+
+library(leaflet)
+library(leafem)
+library(stars)
+library(sf)
+library(raster)
+
+sites_all = read.csv("_data_prod/sites_all.csv")
+
+# rf.predict.ericar.p= read_stars("_data_prod/rf.predict.ericar_prob_wm30_wm.tif", proxy = FALSE,  package="stars")
+rf.values.p = raster("_data_prod/rf.predict.ericar_prob_wm30_wm.tif")
+rasproj2 = raster::aggregate(rf.values.p, fac = 3)
+rasproj2[rasproj2<0.01] = NA
+rasproj2[rasproj2>1] = 1
+rf.predict.ericar.p3 = st_as_stars(rasproj2)
+
+leaflet(sites_all)%>%
+  addProviderTiles("OpenStreetMap.HOT")%>%
+  leafem::addGeoRaster(rf.predict.ericar.p3, opacity = 0.8, colorOptions = colorOptions(palette = colorRampPalette(c("lightblue", "violet", "darkviolet"), space="Lab"))) %>%
+  setView(lng=5.5,lat=45,zoom=6) %>%
+  addCircleMarkers(lng = ~lon_wgs84, lat = ~lat_wgs84, popup = ~date, radius = 0.3, opacity = 0.8, color = "black") 
+
 ## legend
 
 
